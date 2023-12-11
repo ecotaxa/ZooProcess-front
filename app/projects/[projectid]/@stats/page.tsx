86e07c0daef7fe4458cdaa5c1201ba948f08a23d
Galvagno Sébastien <dev@galvagno.info>
2023-12-11 08:22:32 +0100

    sample stat page with component use

--- app/projects/[projectid]/@stats/page.tsx
+++ app/projects/[projectid]/@stats/page.tsx
@@ -1,40 +1,62 @@
 import { Card, CardBody, Spacer } from "@nextui-org/react";
 
+
+import { ArrowDownIcon, ArrowDownTrayIcon, CloudIcon, PlusIcon } from "@heroicons/react/20/solid";
+import React, { FC } from "react";
+
+interface IBoxMessage {
+    children : any // React.ReactNode //React.RefAttributes<SVGSVGElement>
+    title : String
+    subtitle : String
+}
+
+const BoxMessage : FC<IBoxMessage> = (props) => {
+
+    const {children,title,subtitle} = props   
+
+    const IconElement = React.cloneElement(children, {className:"h-16 w-16 text-blue-500"})
+
+    return (
+        <Card>
+        <CardBody>
+            <div className="flex">
+                <div className="flex-none p-6">{IconElement}</div> 
+                    <div className="flex flex-wrap">
+                    <h2>{title}</h2>
+                    <h3>{subtitle}</h3>
+                </div>
+            </div>
+        </CardBody>
+    </Card>
+    )
+}
+
 const Stats = () => {
 
     return (
         <div>
-            <Card>
-                <CardBody>
-                    <div className="flex flex-col">
-                        <span>Icone Plus</span>
-                        <h2>Your project is empty ! Please create samples</h2>
-                        <h3>You can create samples in the data tab of your project.</h3>
-                    </div>
-                </CardBody>
-            </Card>
+            <BoxMessage
+                title="Your project is empty ! Please create samples"
+                subtitle="You can create samples in the data tab of your project."
+            >
+                <PlusIcon/>
+            </BoxMessage>
 
             <Spacer y={10}/>
-            <Card>
-                <CardBody>
-                    <div className="flex flex-col">
-                        <span>Icone Download</span>
-                        <h2>Your project is empty! Please import data.</h2>
-                        <h3>You can import or scan and import data in the import tab of your project.</h3>
-                    </div>
-                </CardBody>
-            </Card>
+            <BoxMessage
+                title="Your project is empty! Please import data."
+                subtitle="You can import or scan and import data in the import tab of your project."
+            >
+                <ArrowDownTrayIcon/>
+            </BoxMessage>
             
             <Spacer y={10}/>
-            <Card>
-                <CardBody>
-                    <div className="flex flex-col">
-                        <span>Icone Cloud</span>
-                        <h2>Your project isn't linked to an EcoTaxa project! Please link it to an existing or new EcoTaxa project.</h2>
-                        <h3>You will then be able to import data to EcoTaxa from EcoPart and classify it in EcoTaxa.</h3>
-                    </div>
-                </CardBody>
-            </Card>
+            <BoxMessage
+                title="Your project isn't linked to an EcoTaxa project! Please link it to an existing or new EcoTaxa project."
+                subtitle="You will then be able to import data to EcoTaxa from EcoPart and classify it in EcoTaxa."
+            >
+                <CloudIcon/>
+            </BoxMessage>
         </div>
     );
 };
