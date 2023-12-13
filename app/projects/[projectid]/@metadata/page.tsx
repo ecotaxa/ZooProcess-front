fed9a65eecae8cf9dd36a5217c7b8a1c90738784
Galvagno Sébastien <dev@galvagno.info>
2023-12-13 11:15:38 +0100

    doing - first version

--- app/projects/[projectid]/@metadata/page.tsx
+++ app/projects/[projectid]/@metadata/page.tsx
@@ -1,10 +1,136 @@
-const Metadata = () => {
+"use client"
+
+import Head from 'next/head';
+
+import { updateProject, useProject } from "@/app/api/projects";
+import { ErrorComponent } from "@/components/ErrorComponent";
+import { MySpinner } from "@/components/mySpinner";
+import { projectForm } from "@/config/formElements";
+import { FC, useEffect, useState } from 'react';
+import { Stack } from '@mui/material';
+
+import { Project as IProject } from '@/app/api/network/zooprocess-api';
+import { useRouter } from 'next/navigation';
+import { MyForm } from '@/components/myForm';
+
+interface pageProps {
+    // params: {
+      projectid: string
+    // }
+  }
+
+const Metadata : FC<pageProps> = (params) => {
+  const router = useRouter();
+
+    const projectId = params.projectid ;
+    console.log("Metadata params: ", params);
+    console.log("Metadata params projectid: ", params.projectid);
+
+    const { project, isLoading, isError } = useProject(projectId)
+    // const [ sampleList, setSampleList ] = useState(project)
+
+    const fillProject = (project:any) : any => { 
+        console.log("project has changed", project);
+        
+        let proj = {
+            "id":project.id,
+            "name": project.name,
+            "drive": project.driveId,
+            "description": project.description,
+            "acronym": "",
+            "ecotaxa_project_title": "",
+            "ecotaxa_project": ""
+      }
+
+      if (project.acronym ) { proj["acronym"] = project.acronym;}
+      if (project.ecotaxa) {
+        proj["ecotaxa_project_title"] = "" //project.ecotaxa.acronym;
+        proj["ecotaxa_project"] = "" // project.acronym;
+      }
+
+      form['value'] = proj;
+      // return proj;
+    }
+
+    let form : any = {}
+    form['forms'] = projectForm; //driveList
+    form['value'] = {};
+    form['title'] = 'New Project';
+    form['subtitle'] = 'Fill metadata about your new project';
+
+  //   let emptyProject = {
+  //     // "project_id": null,
+  //     "name": null, // "Zooscan_",
+  //     "drive": null,
+  //     "acronym": null,
+  //     "description": null,
+  //     "ecotaxa_project_title":null,
+  //     "ecotaxa_project":null
+  // }
+
+  const ProjectForm = () => {
+    if (isLoading) return <MySpinner />
+    if (isError) return <ErrorComponent error={isError}/>
+    // return <SamplesTable projectId={projectId} samples={sampleList}/>
+    
+    const projectMetadata = fillProject(project)
+    // form['value'] = projectMetadata;
 
     return (
-        <div>
+        <MyForm {...form} 
+            onChange={onChange} 
+            onCancel={onCancel}
+        />
+    )
+  }
+
+       
+    // onSubmit
+    const onChange = (value:any) => {
+      console.log("App onChange:", value)
+      // const stringifiedData = useMemo(() => JSON.stringify(value, null, 2), [value]);
+      // stringifiedData = JSON.stringify(value, null, 2);
+
+      // POUR AFFICHAGE DEBUG
+      // setData(JSON.stringify(value, null, 2))
+      // console.log("App onChange:", stringifiedData)
+      console.log("App onChange:", JSON.stringify(value, null, 2));
+
+      updateProject(value);
+    }
+
+  const onCancel = () => {
+      router.back()
+      // router.push({
+      //     pathname: '/projects/',
+      //     // query: { pid: params.id },                                         
+      // })
+  }
+
+
+
+  return (
+        <>
+        <Head>
+            <title>
+            Metadata | ZooProcess
+            </title>
+        </Head>
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+
+        <div className="text-center justify-center">
+            <Stack spacing={3}>
             <h1>Metadata</h1>
-        </div>
+                <ProjectForm/>
+            </Stack>
+            </div>
+        </section>
+        </>
     );
-};
+
+}
+
+
+
 
 export default Metadata;
