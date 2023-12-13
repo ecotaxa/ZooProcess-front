ae93da493683b733b5ca5950af1a76cde1ed75d8
Galvagno Sébastien <dev@galvagno.info>
2023-12-13 11:15:03 +0100

    todo scan

--- app/projects/[projectid]/@scans/page.tsx
+++ app/projects/[projectid]/@scans/page.tsx
@@ -1,8 +1,20 @@
-const Scans = () => {
+import { FC } from "react";
+
+interface pageProps {
+    // params: {
+        projectid: string
+    // }
+}
+
+const Scans : FC<pageProps> = (params) => {
+
+    const projectId = params.projectid ;
+    console.log("Metadata params: ", params);
+    console.log("Metadata params projectid: ", params.projectid);
 
     return (
         <div>
-            <h1>Scans</h1>
+            <h1>{projectId} Scans</h1>
         </div>
     );
 };
