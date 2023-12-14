a9f98a43909923aa52bf53c0ed7cc4b378765511
Galvagno Sébastien <dev@galvagno.info>
2023-12-14 09:02:59 +0100

    improved - debug component

---
+++ components/Debug.jsx
@@ -0,0 +1,43 @@
+
+import {Switch} from "@nextui-org/react";
+import { useState } from "react";
+export const Debug = ({params,title="debug"}) => {
+
+
+
+    const [isSelected, setIsSelected] = useState(true);
+
+    // return (
+    //     <div>
+    //         {JSON.stringify(params)}
+    //     </div>
+    // )
+
+    const ShowError = () => {
+
+        if (isSelected) { return (<></>) }
+
+        return (
+            <div>
+                {JSON.stringify(params)}
+            </div>
+        )
+    }
+
+    return (
+    <>
+        <div className="flex flex-col gap-2">
+            <Switch 
+                isSelected={isSelected} 
+                onValueChange={setIsSelected} 
+                size="sm"
+            >
+                {title}
+            </Switch>
+            {/* <p className="text-small text-default-500">Selected: {isSelected ? "true" : "false"}</p> */}
+        </div>
+
+        <ShowError/>
+    </>
+    );
+}
