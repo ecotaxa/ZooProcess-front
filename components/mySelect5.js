694fe220837e3167b2887ba2e73ed7e42c394f41
Galvagno Sébastien <dev@galvagno.info>
2023-12-20 17:07:23 +0100

    fixed but warning on unselect

--- components/mySelect5.js
+++ components/mySelect5.js
@@ -4,9 +4,9 @@ import { Debug } from "@/Components/Debug";
 
 export function MySelect(props){
 
-    //console.log("MySelect props: ",props);
+    console.log("MySelect props: ", props);
     const [value, setValue] = useState(
-            String(props.value) || "0"
+            String(props.value) || 0
         );
 
 
@@ -14,24 +14,47 @@ export function MySelect(props){
     // const handleChange = (event) => {
         // console.log("typeof event: ", typeof(event));
         // const value = event.target.value;
-        // console.log("handleChange: ", value)
+        console.log("handleChange: ", value)
 
-        if (props.onChange){
-            props.onChange(props.name, value)
+
+        if ( value != "" ){
+            setValue(value);
+            if (props.onChange){
+                props.onChange(props.name, value)
+            }
+            } else {
+            setValue(0)
+            if (props.onChange){
+                props.onChange(props.name, 0)
+            }
         }
-        setValue(value);
     }
+
+    console.log("props.choice: ", props.choice)
+    console.log("value: ", value)
+    const choice = Object.keys(props.choice).map((key) => {
+        console.log("key: ", key, " => ", props.choice[key])
+        return { 
+            'id': String(props.choice[key].id), 
+            'value': props.choice[key].value
+        }
+    })
+
+    console.log("Choice:", choice)
 
     let opt = {
         id: props.name,
-        items: props.choice,
+        // items: props.choice,
+        items: choice,
         label: props.label,
         placeholder: props.placeholder,
-        className:"max-w-xs",
+        className: "max-w-xs",
     }
 
     // if (props.value) { opt['defaultSelectedKeys'] = [props.value]; }
     if (props.required == true) { opt['isRequired'] = true; }
+
+    // if (value) { opt['selectedKeys'] = [value] }
 
     // console.log("OPT:" , opt);
 
@@ -39,10 +62,12 @@ export function MySelect(props){
         <>
         <Debug params={[{props:props},{opt:opt}]} />
         <Select
+            // selectedKeys={[String(value)]}
             selectedKeys={[value]}
-            // onChange={(event) => handleChange(event.target.value)}
+            onChange={(event) => handleChange(event.target.value)}
             // onChange={handleChange}
-            onSelectionChange={handleChange}
+            // onSelectionChange={handleChange}
+            // items= {choice}
             {...opt}
         >
             {(item) => <SelectItem key={item.id}>{item.value}</SelectItem>}
