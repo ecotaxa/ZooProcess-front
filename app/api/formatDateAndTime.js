fc920b7cb50c78d1157ee58a6a1b5dc4a65694a5
Galvagno Sébastien <dev@galvagno.info>
2023-12-29 20:12:24 +0100

    cleanup

--- app/api/formatDateAndTime.js
+++ app/api/formatDateAndTime.js
@@ -4,17 +4,13 @@ export const formatDate = (date /*:string | number | Date */ ) => {
     let localdate = "date";
     const locale /*: LocalesArgument*/ = "fr-FR";
     try {
-        // localdate = new Date(date).toLocaleString();
         let dayoptions /* : DateTimeFormatOptions */ = {
             // weekday: "long",
             year: "numeric",
             month: "numeric",
             day: "numeric",
         };
-        // let day = new Date(date).toLocaleString(locale,dayoptions);
-        // let time=new Date(date).toLocaleTimeString();
-        // localdate = day+"<br/>"+time
-        localdate = new Date(date).toLocaleString(locale,dayoptions);
+        localdate = new Date(date).toLocaleString(locale, dayoptions);
         // console.log("formatDate date:",localdate);
     } catch (error) {
         console.error(error);
@@ -29,8 +25,7 @@ export const formatTime = (date /*:string | number | Date */) => {
     let localdate = "time";
     const locale /*: LocalesArgument*/ = "fr-FR";
     try {
-        
-        localdate=new Date(date).toLocaleTimeString();
+        localdate = new Date(date).toLocaleTimeString();
         // console.log("formatTime time:",localdate);
     } catch (error) {
         console.error(error);
