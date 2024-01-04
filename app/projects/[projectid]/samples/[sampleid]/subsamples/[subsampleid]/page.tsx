ee576802e754c0d6bed93d446e022cc83cdb8147
Galvagno Sébastien <dev@galvagno.info>
2024-01-05 00:02:51 +0100

    fixed subsample argument, now ready to show the detail

--- app/projects/[projectid]/samples/[sampleid]/subsamples/[subsampleid]/page.tsx
+++ app/projects/[projectid]/samples/[sampleid]/subsamples/[subsampleid]/page.tsx
@@ -4,33 +4,39 @@ import { FC } from "react";
 
 
 interface pageProps {
-    // params: {
+    params: {
       projectid: string,
       sampleid: string
       subsampleid: string
-    // }
+    }
   }
 
-export const SubsamplePage : FC<pageProps> = ({projectid,sampleid,subsampleid}) => {
+// const SubsamplePage : FC<pageProps> = ({projectid,sampleid,subsampleid}) => {
+const SubsamplePage : FC<pageProps> = ({params}) => {
 
     // console.log("NewSample params: ", params);
-    console.log("NewSample params projectid: ", params.params.projectid);
-    console.log("NewSample params sampleid: ", params.params.sampleid);
+    // console.log("NewSample params projectid: ", params.params.projectid);
+    // console.log("NewSample params sampleid: ", params.params.sampleid);
+    // console.log("NewSample params sampleid: ", params.params.subsampleid);
 
+    const {projectid,sampleid,subsampleid} = params
 
-    // console.log("NewSample params: ", params);
+    console.log("NewSample params: ", params);
     console.log("NewSample params projectid: ", projectid);
     console.log("NewSample params sampleid: ", sampleid);
     console.log("NewSample params sampleid: ", subsampleid);
 
+
     return (
         <>
-            SubSbample Scan Preview
-            <div><b>project Id: </b> projectid</div>
-            <div><b>sample Id: </b> sampleid</div>
-            <div><b>subsample Id: </b> subsampleid</div>
+            SubSample Scan Preview
+            <div><b>project Id: </b> {projectid}</div>
+            <div><b>sample Id: </b> {sampleid}</div>
+            <div><b>subsample Id: </b> {subsampleid}</div>
         </>
 
     )
 
 }
+
+export default SubsamplePage
