423e6f8ad3a4093c82e1ea4b50c1153c5dd5ec9c
Galvagno Sébastien <dev@galvagno.info>
2024-01-19 11:17:35 +0100

    login button component

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import { useRouter } from "next/navigation";
+
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogTrigger,
+// } from "@/components/ui/dialog";
+// import { LoginForm } from "@/components/auth/login-form";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect",
+  asChild?: boolean;
+};
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild
+}: LoginButtonProps) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    console.log("goto /auth/login")
+    router.push("/auth/login");
+  };
+
+//   if (mode === "modal") {
+//     return (
+//       <Dialog>
+//         <DialogTrigger asChild={asChild}>
+//           {children}
+//         </DialogTrigger>
+//         <DialogContent className="p-0 w-auto bg-transparent border-none">
+//           <LoginForm />
+//         </DialogContent>
+//       </Dialog>
+//     )
+//   }
+
+  return (
+    // <span onClick={onClick} className="cursor-pointer">
+      // {/* {children} */}
+      <Button color="secondary" onPress={onClick} >
+          {children}
+      </Button>
+    // </span>
+  );
+};
