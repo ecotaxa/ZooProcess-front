7fb8fa427a3a1e68e25f4a1e5bd9cf5d337876ca
Galvagno Sébastien <dev@galvagno.info>
2024-02-13 10:20:42 +0100

    timeline composant, need to finish css and add a progress bar to manage X.5 position

---
+++ components/timeline-scan.tsx
@@ -0,0 +1,82 @@
+import { CheckCircleIcon, XCircleIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/20/solid"
+import { FC } from "react"
+
+interface pageProps {
+      current: number
+      done?: number
+  }
+export const Timeline_scan :FC<pageProps> = (props) => {
+
+    const current = props.current // || 0
+    const done = props.done || Math.round(current) - 1
+
+    const list = [
+        { text:"Fill Metadata", checked:true },
+        { text:"Preview", checked:false },
+        { text:"Scan", checked:true },
+        { text:"Process", checked:false },
+        { text:"Check", checked:false },
+    ]
+
+    function Icon({index}) {
+
+        if ( index <= current ) {
+            if ( index <= done ) {
+                return (
+                    <CheckCircleIcon className="h-6 w-6 mx-1 stroke-green-400"/>
+                )
+            }
+            else {
+                return (
+                    <MagnifyingGlassCircleIcon className="h-6 w-6 mx-1 stroke-blue-400"/>
+                    )
+            }
+        } else {
+            return (
+                <XCircleIcon className="h-6 w-6 mx-1 stroke-red-400"/>
+            )
+        }
+        
+    }
+
+    const Items = () => {
+        return (
+        list.map((item, index) => {
+            return (
+                <div key={index} className="flex flex-row   gap-40 border  ">
+                    {/* <div className="flex flex-row   gap-40 border"> */}
+                    <div className="flex-none grow-0  border border-red-400">
+                    {/* {item.checked? ( */ }
+                    {/* { index <= current? (
+                        if ( done && index <= done ) {
+                            <CheckCircleIcon className="h-6 w-6 mx-1 stroke-green-400"/>
+                        } else {
+                            <MagnifyingGlassCircleIcon className="h-6 w-6 mx-1 stroke-blue-400"/>
+                        }
+
+                    ) : (
+                        <XCircleIcon className="h-6 w-6 mx-1 stroke-red-400" color="red"/>
+                    )} */}
+                    <Icon index={index}/>
+                    </div>
+                    <div className="grow  bg-blue-800">
+                        <p className="w-max  text-center">{item.text}</p>
+                    </div>
+                    {/* <div className="grow bg-yellow-200"></div> */}
+                    {/* </div> */}
+                </div>
+            )
+        })
+        )
+    }
+
+    return (
+        <div className="w-full flex flex-nowrap border" > 
+        {/* "grid grid-cols-5 gap-4"> */}
+
+        <Items/>
+        
+        </div>
+    )
+
+}
