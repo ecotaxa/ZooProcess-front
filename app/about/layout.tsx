a1f01e940d76b802c3f6d55ece95ac9416ce61e6
Galvagno Sébastien <dev@galvagno.info>
2024-02-13 10:32:37 +0100

    cleaned

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,17 +1,18 @@
 export default function AboutLayout({
 	children,
-	dashboard,
+	// dashboard,
 }: {
 	children: React.ReactNode;
-	dashboard: React.ReactNode;
+	// dashboard: React.ReactNode;
 }) {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+		<section className="flex items-center justify-center bg-indigo-500" >
+		 {/* flex-row items-center justify-center gap-4 py-8 md:py-10"> */}
 			<div className="inline-block text-center justify-center">
 				{children}
-				Dashbord try ...<br/>
-				{dashboard}
-				Layout
+				{/* Dashbord try ...<br/> */}
+				{/* {dashboard} */}
+				{/* Layout */}
 			</div>
 		</section>
 	);
