152c848bb6ebec5da09405d5ca069b42b8c02d80
Galvagno Sébastien <dev@galvagno.info>
2024-02-13 10:32:51 +0100

    use as a test page ;)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,36 @@
 import { title } from "@/components/primitives";
+import { Timeline_scan } from "@/components/timeline-scan";
 import { Link, Spinner } from "@nextui-org/react";
 
 export default function AboutPage() {
 	return (
+
 		<div>
-			<h1 className={title()}>About</h1>
-			<Spinner/>
-			<div className="flex flex-col gap-4">
-            <Link href={"/blog/123"} className="bg-green-400 w-fit rounded-lg p-4">
-                Single param
-            </Link>
-            <Link href={"blog/a/b"} className="bg-blue-400 w-fit rounded-lg p-4">
-                Multiple Params
-            </Link>
-            <Link href={"blog"} className="bg-amber-400 w-fit rounded-lg p-4">
-                Optional
-            </Link>
-        </div>
+
+
+		
+        <Timeline_scan current={0.5} />
+
+        <Timeline_scan current={1} />
+        <Timeline_scan current={1.5} />
+
+
+
 		</div>
 	);
 }
+
+
+// 	<h1 className={title()}>About</h1>
+		// 	<Spinner/>
+		// 	<div className="flex flex-col gap-4">
+        //     <Link href={"/blog/123"} className="bg-green-400 w-fit rounded-lg p-4">
+        //         Single param
+        //     </Link>
+        //     <Link href={"blog/a/b"} className="bg-blue-400 w-fit rounded-lg p-4">
+        //         Multiple Params
+        //     </Link>
+        //     <Link href={"blog"} className="bg-amber-400 w-fit rounded-lg p-4">
+        //         Optional
+        //     </Link>
+        //  </div>
