70f6b38055822afc5dee01e8df8054d3b89c17d7
Galvagno Sébastien <dev@galvagno.info>
2024-03-16 00:40:05 +0100

    4o4 page fixed error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,38 @@
+"use client";
+
 // import { Link } from "@nextui-org/link";
 
 import Custom404 from "@/app/pages/404";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { useRouter } from "next/navigation";
+import { clientID } from "./api/_auth/[...nextauth]/options";
+// import { Router } from "next/ro";
 // import Link from "next/link";
 
 export default function NotFound() {
 
-    // const router = useRouter();
+    const router = useRouter();
 
     // const back = () => {
     //     router.back();
     // }
 
+    function onClick() {
+        router.back();
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <img src="static/images/404.jpg" alt="404 error" />
+            {/* <img src="static/images/404.jpg" alt="404 error" /> */}
+            <img src="/images/404.jpg" alt="404 error" />
             <div className="text-center">
                 <h1>404 - Page not found</h1>
                 {/* <p>Could not find requested resource</p> */}
                 {/* arg la page n'est pas chargé après le link */}
                 <Link href="/">Return Home</Link>
+                <br/>
+                <Button onClick={onClick}>go back</Button>
                 {/* <Link href="/#home?toto">Return Home</Link> */}
                 {/* <Link href="/projects">Return Home</Link> */}
                 {/* <Button onClick={back}>Go back</Button> */}
