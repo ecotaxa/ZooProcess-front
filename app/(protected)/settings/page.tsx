8f05cb1339176619890c11e8bd39ab6486d342f1
Galvagno Sébastien <dev@galvagno.info>
2024-06-18 15:39:56 +0200

    Formated information on Settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -3,28 +3,100 @@
 import { auth, signOut } from "@/auth"
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
+import { User } from "@nextui-org/react";
+import { Label } from "@radix-ui/react-label";
 import { useRouter } from "next/navigation";
 
 const SettingsPage = async () => {
 
     const session = await auth()
  
+
     if ( session){
         const date = new Date(session.expires)
         const now = new Date()
 
+        console.log("Session expires: ", date)
+        console.log("Session now: ", now)
+
         if ( date < now ){
+            console.log("Session expired")
             await signOut(); 
             const router = useRouter()
             router.push('/auth/login')
+        } else {
+            console.log("Session valid")
         }
     }
     
+
+    const userInfo = () => {
+        let picture = undefined
+        if ( session?.user.image ){
+            picture = session?.user.image
+        }
+
+        const date = new Date(session?.expires)
+
+        return (
+            <div>
+                
+                <User 
+                    name={session?.user.image} 
+                    description= ""
+                    // avatarProps={picture}
+                />
+                <div>
+                    <Label htmlFor="name" className="label">email : </Label>
+                    <p>{session?.user.email}</p>
+                </div>
+
+                <div>
+                    <Label htmlFor="name" className="label">Session Validity :</Label>
+                    <p>{ session?.expires }</p>
+                </div>
+
+            </div>
+        )
+    }
+
+    
+
+    const picture = () => {
+        
+        if (session?.user.image){
+            return ( <li><img src={session?.user.image}/></li> )
+        }
+        return ( <></> )
+    }
+
+
+
     return (
         <div>
-            <p>Settings Page</p>
-            {JSON.stringify(session)}
+            <h1>Settings Page</h1>
+            {/* {userInfo()} */}
+
+            {/* {JSON.stringify(session)} */}
+
+
+            <ul>
+                    {/* <Label htmlFor="name" className="label">Session Validity :</Label> */}
+                    <li>Name: {session?.user.name}</li>
+                    <li>ID: {session?.user.id}</li>
+                    <li>Email: {session?.user.email}</li>
+                    <li>Role: {session?.user.role}</li>
+                    {picture()}
+                    <li></li>
+                    <li>Session expiration date: {session?.expires}</li>
+                    <li>Session token: {session?.user.token}</li>
+            </ul>
+
+            <br/>
+            <hr/>
+            <br/>
             <Button href="projects" as={Link} color="primary">My Projects</Button>
+
             <form action={async () => {
                  "use server";
                  await signOut();   
@@ -40,3 +112,4 @@ const SettingsPage = async () => {
 
 
 export default SettingsPage
+
