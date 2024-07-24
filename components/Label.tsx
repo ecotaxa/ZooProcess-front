dd89552f11a965e4c5209fadc43d91347eb2bc72
Galvagno Sébastien <dev@galvagno.info>
2024-07-24 09:40:59 +0200

    Label component

---
+++ components/Label.tsx
@@ -0,0 +1,9 @@
+
+
+const Label: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <label style={{ display: 'block', marginBottom: '5px' }}>
+      {children}
+    </label>
+  );
+};  
