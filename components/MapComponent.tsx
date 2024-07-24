07e8448587cc30b1e87de9740cee20ecaef8ab7b
Galvagno Sébastien <dev@galvagno.info>
2024-07-24 11:11:46 +0200

    map working in decimal degree

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import L from 'leaflet';
 import { Input, Card, CardBody } from '@nextui-org/react';
+import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
-  const [startLat, setStartLat] = useState(48.8566);
-  const [startLng, setStartLng] = useState(2.3522);
-  const [endLat, setEndLat] = useState(51.5074);
-  const [endLng, setEndLng] = useState(-0.1278);
+  const startPoint = { lat: 48.8566, lng: 2.3522 };
+  const endPoint = { lat: 51.5074, lng: -0.1278 };
+
+  const [startLat, setStartLat] = useState(startPoint.lat);
+  const [startLng, setStartLng] = useState(startPoint.lng);
+  const [endLat, setEndLat] = useState(endPoint.lat);
+  const [endLng, setEndLng] = useState(endPoint.lng);
 
   const positionIcon = new L.Icon({
     iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
@@ -23,7 +27,7 @@ const MapComponent = () => {
         <MapContainer center={[(startLat + endLat) / 2, (startLng + endLng) / 2]} zoom={5} style={{ height: '400px', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
           <Marker position={[startLat, startLng]} icon={positionIcon}>
             <Popup>Start Position</Popup>
@@ -34,29 +38,30 @@ const MapComponent = () => {
           <Polyline positions={[[startLat, startLng], [endLat, endLng]]} color="red" />
         </MapContainer>
         <Input 
-          label="Start Latitude"
-          type="number"
-          value={startLat.toString()} 
-          onChange={(e) => setStartLat(parseFloat(e.target.value))}
+        label="Start Latitude"
+        type="number"
+        value={startLat.toString()} 
+        onChange={(e) => setStartLat(parseFloat(e.target.value))}
         />
         <Input 
-          label="Start Longitude"
-          type="number"
-          value={startLng.toString()} 
-          onChange={(e) => setStartLng(parseFloat(e.target.value))}
+        label="Start Longitude"
+        type="number"
+        value={startLng.toString()} 
+        onChange={(e) => setStartLng(parseFloat(e.target.value))}
         />
         <Input 
-          label="End Latitude"
-          type="number"
-          value={endLat.toString()} 
-          onChange={(e) => setEndLat(parseFloat(e.target.value))}
+            label="End Lati   tude"
+            type="number"
+            value={endLat.toString()} 
+            onChange={(e) => setEndLat(parseFloat(e.target.value))}
         />
         <Input 
-          label="End Longitude"
-          type="number"
-          value={endLng.toString()} 
-          onChange={(e) => setEndLng(parseFloat(e.target.value))}
+        label="End Longitude"
+        type="number"
+        value={endLng.toString()} 
+        onChange={(e) => setEndLng(parseFloat(e.target.value))}
         />
+
       </CardBody>
     </Card>
   );
