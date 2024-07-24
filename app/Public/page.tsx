074f45ec941d09dbfe8ca2fb5d0133a3a4a72292
Galvagno Sébastien <dev@galvagno.info>
2024-07-24 09:41:34 +0200

    the test page

--- app/Public/page.tsx
+++ app/Public/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import MapComponent from "@/components/MapComponent";
+
 const Public = () => {
 
     return (
         <div>
             <h1>Public</h1>
+            <MapComponent></MapComponent>
         </div>
     );
 };
