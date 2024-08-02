fb200885fe5f1d4474ab118c8500fc09833b75b1
Galvagno Sébastien <dev@galvagno.info>
2024-08-02 17:06:11 +0200

    bon la vue de l'Artique fonctionne mais c'est une vue satellite

--- components/ArcticMap.tsx
+++ components/ArcticMap.tsx
@@ -1,32 +1,61 @@
 import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'proj4';
+import 'proj4leaflet';
 
-const ArcticMap = () => {
+// Définition de la projection EPSG:3413
+const crs = new (L as any).Proj.CRS(
+  'EPSG:3413',
+  '+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs',
+  {
+    resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+    origin: [-4194304, 4194304],
+    bounds: L.bounds(
+      [-4194304, -4194304],
+      [4194304, 4194304]
+    )
+  }
+);
+
+// Composant pour initialiser la vue de la carte
+const MapInitializer: React.FC = () => {
+  const map = useMap();
   useEffect(() => {
-    // Créer la carte
-    const map = L.map('map').setView([89, 0], 4);
+    map.setView([90, 0], 3);
+  }, [map]);
+  return null;
+};
 
-    // Ajouter la couche de tuiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-
-    // Ajouter les marqueurs
-    const marker1 = L.marker([89, 2.45]).addTo(map);
-    const marker2 = L.marker([88.7, 0.1]).addTo(map);
-
-    // Ajouter des popups aux marqueurs
-    marker1.bindPopup("Point 1: 89°N, 2.45°E");
-    marker2.bindPopup("Point 2: 88.7°N, 0.1°E");
-
-    // Nettoyage
-    return () => {
-      map.remove();
-    };
-  }, []);
-
-  return <div id="map" style={{ height: '400px', width: '100%' }} />;
+const ArcticMap: React.FC = () => {
+  return (
+    <MapContainer
+      center={[90, 0]}
+      zoom={3}
+      style={{ height: '600px', width: '100%' }}
+      crs={crs}
+      maxZoom={8}
+      minZoom={0}
+    >
+      <MapInitializer />
+      <TileLayer
+        url='https://map1.vis.earthdata.nasa.gov/wmts-arctic/MODIS_Terra_CorrectedReflectance_TrueColor/default/{time}/{tilematrixset}/{z}/{y}/{x}.jpg'
+        attribution='Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.'
+        maxZoom={8}
+        minZoom={0}
+        tileSize={512}
+        time="2023-06-01"
+        tilematrixset="EPSG3413_250m"
+      />
+      <Marker position={[89, 2.45]}>
+        <Popup>Point 1: 89°N, 2.45°E</Popup>
+      </Marker>
+      <Marker position={[88.7, 0.1]}>
+        <Popup>Point 2: 88.7°N, 0.1°E</Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default ArcticMap;
