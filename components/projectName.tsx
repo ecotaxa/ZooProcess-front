e3f24ae2f9d4763e33c632a12564c762b77c650b
Galvagno Sébastien <dev@galvagno.info>
2025-02-12 07:56:24 +0100

    cleanup

--- components/projectName.tsx
+++ components/projectName.tsx
@@ -1,8 +1,4 @@
-import { useProject } from "@/app/api/projects";
-import { MySpinner } from "./mySpinner";
-import { ErrorComponent } from "./ErrorComponent";
 import * as api from "@/app/api/network/zooprocess-api";
-import { th } from "date-fns/locale";
 import { Project } from "@/app/api/network/interfaces";
 
 
@@ -25,17 +21,11 @@ async function ProjectName ({
     id,
   }: ProjectNameProps) : Promise<JSX.Element> {
     
-    // const { project, isLoading, isError } = useProject(id)
-
     const project = await getProject(id);
     
-    // if (isLoading) return <MySpinner />
-    // if (isError) return <ErrorComponent error={isError}/>
-  
   
 
     return (
-    //   <div className="bg-red-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-red-500">
         <div>
             <h1>Project</h1>
             <br/>
