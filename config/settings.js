7395c72a25c6301b0cc35c04c6369cbce4c9d37c
Galvagno Sébastien <dev@galvagno.info>
2025-03-17 18:02:43 +0100

    add function to debug Form or put it in lazy mode to fill it

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,12 @@
 
 // export let debug = false
-// export var debug = true
-export let debug = process.env.DEBUG || false
+export var debug = true
+// export let debug = process.env.DEBUG || false
+
+export var debugForm = [] 
+debugForm['project'] = false
+
+export const  isDebugFormEnabled = (form) => {
+    return debugForm.includes(form)
+}
+
