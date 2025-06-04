0e4e0af94c786ea3084f0d603416da7ac47a62da
Galvagno Sébastien <dev@galvagno.info>
2025-06-04 10:23:36 +0200

    cleaned

--- app/(protected)/projects/[projectid]/new/newSampleForm.tsx
+++ app/(protected)/projects/[projectid]/new/newSampleForm.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import Head from 'next/head';
-// import { Box, Container, Stack, Typography } from '@mui/material';
 import { FC } from "react";
 
-// import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-// import { ProjectsTable } from 'src/sections/projects/projects-table';
+
 import { MyForm } from '@/components/myForm';
 import { inputFormElements, inputFormElementsMapTest } from '@/config/formElements';
 import { useState } from 'react';
@@ -19,55 +17,13 @@ import { CustomError, removeDigest, removeDigestFromError, removeDigestSafe } fr
  
 
 
-
-
-// const testData = {
-//     //"sample_name":"dyfamed_wp2_2023_biotom_sn001",
-//     "sample_id": "dyfamed_wp2_2023_biotom_sn001",
-//     "scientific_program": "ZooProcess",
-//     "latitude_ns": 1,
-//     "station_id": "dyfamed",
-//     "bottom_depth": "2400",
-//     "sampling_date": "2022-11-13T00:13:40.000Z",
-//     // "sampling_date": "2023-10-09T09:13:40.000Z",
-//     "Latitude_degree": "43",
-//     "longitude_degree": "7",
-//     "latitude_minute": "25",
-//     "longitude_minute": "51",
-//     "longitude_ew": 1,
-//     "tow_type": "1",
-//     "net_sampling_type": "WP2",
-//     "net_mesh": "200",
-//     "net_opening_surface": "0.25",
-//     "minimum_depth": "1",
-//     "maximum_depth": "1",
-//     "quality_flag_for_depth_measurement": "2",
-//     "ship_speed": "99999",
-//     "cable_speed": "99999",
-//     "cable_length": "99999",
-//     "cable_angle_from_vertical": "45",
-//     "sampling_duration": "5"
-// }
-
 const forms = [
-    // sampleid_formElements, 
-
     inputFormElements, 
-    // inputFormElementsMapTest
-    
-    // inputFormElements_tow_type_vertical, 
-    // fraction_inputFormElments
 ]
 
 interface pageProps {
-    // params: {
-        // projectid: string
         project: Project
-    // }
 }
-
-// const NewSample : FC<pageProps> = (params) => {
-    // const NewSampleForm : FC<pageProps> = ({projectid}) => {
         const NewSampleForm : FC<pageProps> = ({project}) => {
 
     const router = useRouter()
@@ -76,9 +32,7 @@ interface pageProps {
     // console.log("NewSample params projectid: ", projectid);
     // console.log("NewSample params projectid: ", params.projectid);
 
-    // const projectid = router.query.projectid //as string
     const projectId = project.id;
-    // const projectId = params.projectid;
 
     const emptyData = {
         "scientific_program": "ZooProcess",
@@ -93,9 +47,7 @@ interface pageProps {
 
 
 
-    // const [stringifiedData,setData] = useState(JSON.stringify(testData, null, 2))
     const [stringifiedData,setData] = useState("")
-    // var stringifiedData = "" ;
 
     const prepareData = (data:any) => {
 
@@ -109,13 +61,10 @@ interface pageProps {
 
     const onChange = async (value:any) => {
         console.log("App onChange:", value)
-        // const stringifiedData = useMemo(() => JSON.stringify(value, null, 2), [value]);
-        // stringifiedData = JSON.stringify(value, null, 2);
 
         setData(JSON.stringify(value, null, 2))
         console.log("App onChange:", stringifiedData)
 
-        // const newData = prepareData(value)
 
         const data = {
             name: `${projectId}_${value.sample_id}`, //"Sample XXXX",
@@ -124,7 +73,6 @@ interface pageProps {
         }
 
         console.log("newData: ", data);
-        // try {
 
             console.log("----- projectId : ",projectId);
             console.log("----- name : ", data.name);
@@ -137,27 +85,12 @@ interface pageProps {
                 projectId, // : params.params.projectid, 
                 data
             })
-            // .then((response) => {
-            //     console.log("Go To the sample page" )
-            //     router.push(`samples/${response.data.id}`)
-            // })
-            // .catch((error) => {
-            //     return Promise.reject(error)
-            // })
 
-        // }
-        // catch (e:any){
-        //     console.log(e);
-            
-        // }
     }
 
     const onCancel = () => {
         router.back()
-        // router.push({
-        //     pathname: '/projects/[projectid]',
-        //     query: { projectid: projectid },                                         
-        // })
+
     }
 
     return (
@@ -171,33 +104,11 @@ interface pageProps {
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 
             <div className="text-center justify-center">
-                {/* <Stack spacing={3}> */}
-                    {/* <Typography variant="h4">
-                    Sample Metadata to project {projectId}
-                    </Typography> */}
+
                     <MyForm 
                         {...form} 
-                        // project={projectId}
                         project={project.name}
-                        // onChange={onChange} 
-                        // onChange={async (value:any) => {
-                        //     onChange(value)
-                        //      .then((response: { data: { id: any; }; }) => {
-                        //         console.debug("++++++++++++++++++++++++++++++++++")
-                        //         console.debug("response: ", response)
-                        //         console.log("Go To the sample page" )
-                        //         router.push(`samples/${response.data.id}`)
-                        //         return Promise.resolve(response)
-                        //     })
-                        //     .catch((error: any) => {
-                        //         console.debug("+++==================++++++++++++")
-                        //         console.error("Error adding sample:", error)
-                        //         console.log("Error adding sample:", JSON.stringify(error.message))
-                        //         console.log("Error adding sample:", JSON.stringify(error))
-                        //         // return Promise.reject(error)
-                        //         throw error
-                        //     })
-                        // }}
+
                         onChange={async (value:any) => {
                             try {
                                 const response = await onChange(value);
@@ -214,36 +125,8 @@ interface pageProps {
 
                                 console.log("Full error details:", JSON.stringify(error, null, 2));
 
-
-                                // const errorMessage = typeof error === 'object' ? error.message : error;
-                                // // throw new Error(errorMessage);
-
-                                // const errorMessage = error?.status === "error" ? error.message : 
-                                // typeof error === 'object' ? error.message : 
-                                // error;
-                                // console.log("Error adding sample:", errorMessage);
-
-                                // Extract the actual error message
-                                // const errorMessage = error.message || error.toString();
-
-                                // throw errorMessage
-
-                                // throw String(error);
-                                // Promise.reject(error)
-                                // throw error.toString();
-                                // const errorObj = JSON.parse(error.message);
-                                // throw errorObj.message;
-                                // const cleanedObject = removeDigest(error);
-
                                 const errorObject = { message: "An error occurred", digest: "3967790229" };
-                                // const cleanedObject = removeDigest(errorObject);
-                                // const cleanedObject = removeDigestSafe(errorObject);
-
                                 console.log("typeof error: ", typeof error);
-                                // console.log("typeof error: ",  error is Error);
-
-
-                                // const cleanedObject = removeDigestSafe(error);
                                 const cleanedObject = removeDigestFromError(error);
 
                                 console.log("Cleaned Object:", cleanedObject);
@@ -253,7 +136,6 @@ interface pageProps {
                             }
                         }}
                         onCancel={onCancel}/>
-                {/* </Stack> */}
             </div>
         </section>
         </>
