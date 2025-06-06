642a4c2cb1fd738ac7ea74c325f488f4ac1e5989
grololo06 <laurent.salinas@laposte.net>
2025-06-06 22:45:31 +0200

    proper naming of new subsamples

--- app/(protected)/projects/[projectid]/samples/[sampleid]/subsamples/new/SubSampleForm.tsx
+++ app/(protected)/projects/[projectid]/samples/[sampleid]/subsamples/new/SubSampleForm.tsx
@@ -133,7 +133,7 @@ const SubSampleForm :  FC<pageProps> = ({sample, user})  => {
         // setData(JSON.stringify(value, null, 2))
         
         const data = {
-            name: `${projectid}_${value.sample_id}`,
+            name: `${sampleid}_${value.scan_id}`,
             metadataModelId: "6565df171af7a84541c48b20",
             data:value,
         }
