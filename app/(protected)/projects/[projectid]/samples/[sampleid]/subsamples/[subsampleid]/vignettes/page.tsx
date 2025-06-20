d18d1c71ab700886ccdfae0b5b58a538caa8cfc9
grololo06 <laurent.salinas@laposte.net>
2025-06-20 08:40:34 +0200

    call vignette-related backend with proper paths

--- app/(protected)/projects/[projectid]/samples/[sampleid]/subsamples/[subsampleid]/vignettes/page.tsx
+++ app/(protected)/projects/[projectid]/samples/[sampleid]/subsamples/[subsampleid]/vignettes/page.tsx
@@ -5,8 +5,17 @@ import { fetchVignetteData } from '@/components/lib/api';
 import dynamic from 'next/dynamic'
 const VignetList = dynamic(() => import('@/components/VignetList'), { ssr: false })
 
-export default async function TestPage() {
-  const { data, folder } = await fetchVignetteData();
+interface PageProps {
+  params: {
+    projectid: string;
+    sampleid: string;
+    subsampleid: string;
+  };
+}
+
+export default async function TestPage({ params }: PageProps) {
+  const { projectid, sampleid, subsampleid } = params;
+  const { data, folder } = await fetchVignetteData(projectid, sampleid, subsampleid);
 
   return (
     <main className="flex h-screen w-screen overflow-hidden">
